305fe71820b543c4a83858a3655aeae76174f9b1
atakantekoglu <atakantekoglu@gmail.com>
2022-06-15 01:47:32 +0300

    [atakan] feat: add social media section

--- src/components/partials/Navbar.js
+++ src/components/partials/Navbar.js
@@ -1,13 +1,28 @@
 import React from 'react';
-import Logo from '../../assets/images/logo.svg';
 import {Link} from "react-router-dom";
+
+import SocialLinks from "../../assets/static/socialLinks";
+import Logo from '../../assets/images/logo.svg';
 
 const Navbar = () => {
     return (
-        <nav>
+        <nav className="flex flex-row justify-between items-center">
             <Link to="/">
                 <img src={Logo} alt="logo" width={64} height={64}/>
             </Link>
+            <ul className="flex flex-row">
+                {
+                    SocialLinks.map(({id,name,icon,url}) => {
+                        return (
+                            <li key={id} className="mr-2 last:mr-0">
+                                <a href={url} target="_blank" rel="noopener noreferrer">
+                                    <img src={icon} alt={name} width={24} height={24}/>
+                                </a>
+                            </li>
+                        )
+                    })
+                }
+            </ul>
         </nav>
     );
 };
