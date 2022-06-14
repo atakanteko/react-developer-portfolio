8495715cd87d83433c310870917ce4fb5e89c974
atakantekoglu <atakantekoglu@gmail.com>
2022-06-15 01:47:18 +0300

    [atakan] feat: create a file that contains all social media links and icons

---
+++ src/assets/static/socialLinks.js
@@ -0,0 +1,26 @@
+import Github from '../../assets/icons/github-icon.svg';
+import Linkedin from '../../assets/icons/linkedin-icon.svg';
+import Twitter from '../../assets/icons/twitter-icon.svg';
+
+const socialLinks = [
+    {
+        id : 1,
+        name: 'Github',
+        icon: Github,
+        url: 'https://github.com/'
+    },
+    {
+        id : 2,
+        name: 'Linkedin',
+        icon: Linkedin,
+        url: 'https://www.linkedin.com/in/'
+    },
+    {
+        id : 3,
+        name: 'Twitter',
+        icon: Twitter,
+        url: 'https://twitter.com/'
+    }
+];
+
+export default socialLinks;
