819f4503ddca7a6b7fb2d651477f92883da6ab31
atakantekoglu <atakantekoglu@gmail.com>
2022-06-15 01:18:09 +0300

    [atakan] feat: add react router and create layout component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
-
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 function App() {
   return (
-    <div className="App">
-      <h1>
-        <span>Hello</span>
-      </h1>
+    <div>
+        <Routes>
+            <Route path="/" element={<Home />} />
+        </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter} from "react-router-dom";
+
+import Layout from "./components/layout/Layout";
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <BrowserRouter>
+        <Layout>
+            <App />
+        </Layout>
+    </BrowserRouter>
 );
