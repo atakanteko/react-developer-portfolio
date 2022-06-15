7a20d109bd42bb5be702d2305f0d31ef39c62b66
atakantekoglu <atakantekoglu@gmail.com>
2022-06-15 11:09:39 +0300

    [atakan] feat: align profile font and image in the middle v2

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,9 +4,10 @@ import Profile from "../components/Profile";
 
 const Home = () => {
     return (
-        <div className="h-screen py-10">
-            <Navbar />
-            <Profile />
+        <div className="h-screen py-10 flex flex-col justify-between">
+                <Navbar />
+                <Profile />
+                <div></div>
         </div>
     );
 };
