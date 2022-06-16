5c146ed1282fd61fe8f7a99b008e88f8d4c14e5c
atakantekoglu <atakantekoglu@gmail.com>
2022-06-16 12:48:18 +0300

    [atakan] feat: re-organize menu

--- src/assets/static/menu.js
+++ src/assets/static/menu.js
@@ -1,17 +1,8 @@
 const menu = [
-
-    {
-        name: 'About',
-        url: '/about',
-    },
     {
         name: 'Projects',
         url: '/projects',
-    },
-    {
-        name: 'Contact',
-        url: '/contact',
-    },
+    }
 ];
 
 export default menu;
